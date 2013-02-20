e4673072703f44fbea60fb0242091461b6addddd
RayDemo <john.chr.stewart@gmail.com>
2013-02-19 22:55:06 -0500

    Update Mock_Py_TP.py
    Making a change that has to be approved.

--- Mock_Py_TP.py
+++ Mock_Py_TP.py
@@ -8,8 +8,10 @@ import time
 Test = ActiveStat("MVB_FDG_APPROACH_LO_FQT_TP")
 Test.covers("R000001", "R000002","R000003","R000004","R000005")
 
+# Making a change that requires approval, approve me!
+
 def testCase5(self):
-	Test.testing("R000001", "R000005")
+	Test.testing("R000001", "R000005", "R000004")
 	while(BusMon.get(1553types.Address("A12R2SA12W2")) != 0x0102)
 		time.wait(1)
 	iVDE.PIMFD.push("R4", "T4", "R2")
